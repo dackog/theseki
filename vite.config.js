55b245285d560bd8a840022dc03bd48d797a9749
Kogure <GitHub>
2026-03-26 19:56:07 +0900

    VITE修正

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages のサブパス deploy 時は以下を追加:
-  // base: '/theseki/',
+  base: '/theseki/',
 });
